6924866005ec9ad2e1f0e9c538f32241c7f2a652
Serhat KARATAŞLI <serhatkaratasli790@gmail.com>
2023-05-30 14:17:09 +0300

    Interfaceler(repository, service ve unitofwork) yazıldı

---
+++ NlayerApi.Core/IRepositories/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NlayerApi.Core.IRepositories
+{
+    public  interface IGenericRepository<T> where T:class
+    {
+        IQueryable<IEnumerable<T>> GetAll();
+        Task<T> GetByIdAsync(int id);
+        Task<bool> Any(Expression<Func<T, bool>> expression);
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        void Update(T entity);
+    }
+}
---
+++ NlayerApi.Core/IServices/IService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NlayerApi.Core.IServices
+{
+    public interface IService<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task RemoveAsync(T entity);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+        Task UpdateAsync(T entity);
+    }
+}
