85bc35e33114f4f7aaec9e1d4a020eaf07244ecb
Serhat KARATAŞLI <serhatkaratasli790@gmail.com>
2023-05-31 08:55:42 +0300

    service katmanı hazırlandı

---
+++ NlayerApi.Service/Services/Service.cs
@@ -0,0 +1,70 @@
+﻿using NlayerApi.Core.IRepositories;
+using NlayerApi.Core.IServices;
+using NlayerApi.Core.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NlayerApi.Service.Services
+{
+    public class Service<T> : IService<T> where T : class
+    {
+        private readonly IGenericRepository<T> _repository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public Service(IGenericRepository<T> repository, IUnitOfWork unitOfWork)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveRangeAsync(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
