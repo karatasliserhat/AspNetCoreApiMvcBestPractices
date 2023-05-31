ddc4d741d035bb2eb7edd5a1f3531305a5a0560b
Serhat KARATAŞLI <serhatkaratasli790@gmail.com>
2023-05-31 16:43:37 +0300

    api restfull uyarı giderildi

--- NlayerApi.RestFull/Program.cs
+++ NlayerApi.RestFull/Program.cs
@@ -62,7 +62,7 @@ return forecast;
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+internal record WeatherForecast(DateTime Date, int TemperatureC, string Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
