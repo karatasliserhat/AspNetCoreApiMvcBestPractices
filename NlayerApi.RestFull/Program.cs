ca8c3c6bb33c8e45579e36eeb795a871bcb16806
Serhat KARATAŞLI <serhatkaratasli790@gmail.com>
2023-05-31 16:32:11 +0300

    api restfull katmanına swagger eklendi

--- NlayerApi.RestFull/Program.cs
+++ NlayerApi.RestFull/Program.cs
@@ -25,9 +25,19 @@ builder.Services.AddDbContext<AppDbContext>(opts =>
     });
 });
 
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+app.UseSwagger();
+app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
@@ -38,16 +48,17 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+var forecast = Enumerable.Range(1, 5).Select(index =>
+    new WeatherForecast
+    (
+        DateTime.Now.AddDays(index),
+        Random.Shared.Next(-20, 55),
+        summaries[Random.Shared.Next(summaries.Length)]
+    ))
+    .ToArray();
+return forecast;
+})
+.WithName("GetWeatherForecast");
 
 app.Run();
 
